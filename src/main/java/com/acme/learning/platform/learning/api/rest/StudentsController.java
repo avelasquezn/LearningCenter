bb36f20d3cd88fff42c79951be861b8ba5175739
Angel Velasquez <avelasquezn@gmail.com>
2023-05-25 07:44:22 -0500

    feat: added student inbound service.

---
+++ src/main/java/com/acme/learning/platform/learning/api/rest/StudentsController.java
@@ -0,0 +1,51 @@
+package com.acme.learning.platform.learning.api.rest;
+
+import com.acme.learning.platform.learning.domain.service.StudentService;
+import com.acme.learning.platform.learning.mapping.StudentMapper;
+import com.acme.learning.platform.learning.resource.CreateStudentResource;
+import com.acme.learning.platform.learning.resource.StudentResource;
+import com.acme.learning.platform.learning.resource.UpdateStudentResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class StudentsController {
+    private StudentService studentService;
+
+    private StudentMapper mapper;
+
+
+    public StudentsController(StudentService studentService, StudentMapper mapper) {
+        this.studentService = studentService;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public Page<StudentResource> getAllStudents(Pageable pageable) {
+        return mapper.modelListPage(studentService.getAll(), pageable);
+    }
+
+    @GetMapping("{studentId}")
+    public StudentResource getStudentById(@PathVariable Long studentId) {
+        return mapper.toResource(studentService.getById(studentId));
+    }
+
+    @PostMapping
+    public ResponseEntity<StudentResource> createStudent(CreateStudentResource resource) {
+        return new ResponseEntity<>(mapper.toResource(studentService.create(mapper.toModel(resource))), HttpStatus.CREATED);
+    }
+
+    @PutMapping("{studentId}")
+    public StudentResource updateStudent(@PathVariable Long studentId,
+                                         @RequestBody UpdateStudentResource resource) {
+        return mapper.toResource(studentService.update(studentId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{studentId}")
+    public ResponseEntity<?> deleteStudent(@PathVariable Long studentId) {
+        return studentService.delete(studentId);
+    }
+}
