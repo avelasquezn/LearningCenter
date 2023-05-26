54638e269e85f896bbf24be9f2f44385a137d161
Angel Velasquez <pcisavel@upc.edu.pe>
2023-05-25 22:24:07 -0500

    feat: added endpoint path for student resource.

--- src/main/java/com/acme/learning/platform/learning/api/rest/StudentsController.java
+++ src/main/java/com/acme/learning/platform/learning/api/rest/StudentsController.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/students")
 public class StudentsController {
     private StudentService studentService;
 
