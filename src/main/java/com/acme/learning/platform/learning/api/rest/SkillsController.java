57fded9513e7ab0f51038754060cc7d28f6ae815
Angel Velasquez <avelasquezn@gmail.com>
2023-05-30 09:10:46 -0500

    feat: added behavior for skill inbound service.

--- src/main/java/com/acme/learning/platform/learning/api/rest/SkillsController.java
+++ src/main/java/com/acme/learning/platform/learning/api/rest/SkillsController.java
@@ -1,16 +1,50 @@
 package com.acme.learning.platform.learning.api.rest;
 
 import com.acme.learning.platform.learning.domain.service.SkillService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.acme.learning.platform.learning.mapping.SkillMapper;
+import com.acme.learning.platform.learning.resource.CreateSkillResource;
+import com.acme.learning.platform.learning.resource.SkillResource;
+import com.acme.learning.platform.learning.resource.UpdateSkillResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/skills", produces = "application/json")
 public class SkillsController {
     private final SkillService skillService;
+    private  final SkillMapper mapper;
 
 
-    public SkillsController(SkillService skillService) {
+    public SkillsController(SkillService skillService, SkillMapper mapper) {
         this.skillService = skillService;
+        this.mapper = mapper;
     }
+
+    @GetMapping
+    public Page<SkillResource> getAllSkills(Pageable pageable) {
+        return mapper.modelListPage(skillService.getAll(), pageable);
+    }
+
+    @GetMapping("{skillId}")
+    public SkillResource getSkillById(@PathVariable Long skillId) {
+        return mapper.toResource(skillService.getBydId(skillId));
+    }
+
+    @PostMapping
+    public SkillResource createSkill(@RequestBody CreateSkillResource resource) {
+        return mapper.toResource(skillService.create(mapper.toModel(resource)));
+    }
+
+    @PutMapping("{skillId}")
+    public SkillResource updateSkill(@PathVariable Long skillId, @RequestBody UpdateSkillResource resource) {
+        return mapper.toResource(skillService.update(skillId, mapper.toModel(resource)));
+    }
+
+    @DeleteMapping("{skillId}")
+    public ResponseEntity<?> deleteSkill(@PathVariable Long skillId) {
+        return skillService.delete(skillId);
+    }
+
 }
