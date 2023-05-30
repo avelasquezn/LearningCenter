5edea51be0c84bc8d5a23ca771ab4e9b5d853742
Angel Velasquez <avelasquezn@gmail.com>
2023-05-30 07:53:19 -0500

    feat: added behavior to skill.

--- src/main/java/com/acme/learning/platform/learning/domain/model/Skill.java
+++ src/main/java/com/acme/learning/platform/learning/domain/model/Skill.java
@@ -1,6 +1,8 @@
 package com.acme.learning.platform.learning.domain.model;
 
 import com.acme.learning.platform.shared.domain.model.AuditModel;
+import com.acme.learning.platform.shared.exception.ResourceNotFoundException;
+import com.acme.learning.platform.shared.exception.ResourceValidationException;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -30,7 +32,25 @@ public class Skill extends AuditModel {
     // Relationships
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "skill")
-    private Set<Criterion> criteria = new HashSet<>();
+    private Set<Criterion> criteria;
 
+    public Skill addCriterion(String criterionName) {
+        if (criteria == null) {
+            criteria = new HashSet<>();
+        }
+
+        // Validate Criterion Name Uniqueness for Skill
+        if(!criteria.isEmpty()) {
+            if (criteria.stream().anyMatch(criterion -> criterion.getName().equals(criterionName)))
+                throw new ResourceValidationException("Criterion", "A criterion with the same name already exists.");
+        }
+
+        // Add Criterion to Skill
+        criteria.add(new Criterion()
+                .withName(criterionName)
+                .withSkill(this));
+
+        return this;
+    }
 
 }
