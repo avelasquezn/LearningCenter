b7329a3c3d08377a6d83e3d60d9a984a753cc16b
Angel Velasquez <avelasquezn@gmail.com>
2023-06-06 08:02:18 -0500

    chore: added testing and documentation dependencies.

--- src/main/java/com/acme/learning/platform/LearningCenterApplication.java
+++ src/main/java/com/acme/learning/platform/LearningCenterApplication.java
@@ -1,9 +1,7 @@
 package com.acme.learning.platform;
 
-import org.hibernate.annotations.Filter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
