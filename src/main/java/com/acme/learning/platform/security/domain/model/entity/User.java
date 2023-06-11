65ec650423e22ba01231b10618c86ffcd418017a
Angel Velasquez <pcisavel@upc.edu.pe>
2023-06-10 21:45:38 -0500

    fix: removed extension from AuditModel

--- src/main/java/com/acme/learning/platform/security/domain/model/entity/User.java
+++ src/main/java/com/acme/learning/platform/security/domain/model/entity/User.java
@@ -19,7 +19,7 @@ import java.util.Set;
 @With
 @Entity
 @Table(name = "users")
-public class User extends AuditModel {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
