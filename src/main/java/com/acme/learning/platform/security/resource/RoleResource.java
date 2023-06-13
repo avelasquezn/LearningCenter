051ab4e79fdd9b9193ebfaa1c54a1bd5b6cb39be
Angel Velasquez <avelasquezn@gmail.com>
2023-06-13 07:54:26 -0500

    feat: added user and role data transfer object types.

---
+++ src/main/java/com/acme/learning/platform/security/resource/RoleResource.java
@@ -0,0 +1,13 @@
+package com.acme.learning.platform.security.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@With
+public class RoleResource {
+    private Long id;
+    private String name;
+}
